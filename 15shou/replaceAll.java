ecd818f13cbc76a79c5e3b39c89e0783deaac965
Yunosuke Sato <yunosukesato04@gmail.com>
2022-10-03 10:45:17 -0700

    added gitignore

--- 15shou/replaceAll.java
+++
@@ -1,7 +0,0 @@
-public class replaceAll {
-    public class static void main(String[] args) {
-        String s = "abc,def:ghi";
-        String w = s.replaceAll("[beh]","X");
-        System.out.println(w);
-    }
-}
--- 15shou/stringBuilder.java
+++
@@ -1,9 +0,0 @@
-public class stringBuilder {
-    public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 10000; i++) {
-            sb.append("Java");
-        }
-        String s = sb.toString();
-    }
-}
